147a4756d28fd32a71a8f292d0206a5cda303e1a
IweZix <IweZix@users.noreply.github.com>
2023-12-27 17:02:52 +0100

    update: loadCommands.js

--- loaders/loadCommands.js
+++ loaders/loadCommands.js
@@ -7,17 +7,41 @@ module.exports = (client) => {
 
     let count = 0;
     const dirsCommands = readdirSync('./commands/');
+    console.log(`Commands`);
 
     for (const dir of dirsCommands) {
+
         const fileDirs = readdirSync(`./commands/${dir}/`).filter(f => f.endsWith(".js"));
-        // console.log(`📂 ${dir}`);
-        for (const file of fileDirs) {
-            const command = require(`../commands/${dir}/${file}`);
-            // console.log(`└── ${command.data.name}`);
-            client.commands.set(command.data.name, command);
-            count++;
+        if (dir === dirsCommands[dirsCommands.length - 1]) {
+
+            console.log(`└── 📂 ${dir}`);
+
+            for (const file of fileDirs) {
+
+                const command = require(`../commands/${dir}/${file}`);
+                if (file === fileDirs[fileDirs.length - 1]) {
+                    console.log(`    └── ${file}`);
+                } else {
+                    console.log(`    ├── ${file}`);
+                }
+                client.commands.set(command.data.name, command);
+                count++;
+            }
+        } else {
+            console.log(`├── 📂 ${dir}`);
+
+            for (const file of fileDirs) {
+
+                const command = require(`../commands/${dir}/${file}`);
+                if (file === fileDirs[fileDirs.length - 1]) {
+                    console.log(`│   └── ${file}`);
+                } else {
+                    console.log(`│   ├── ${file}`);
+                }
+                client.commands.set(command.data.name, command);
+                count++;
+            }
         }
     };
-
     logger.info(`[Commands] => ${count} logged commands`);
 }
